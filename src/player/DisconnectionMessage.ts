1f841f74d7f7612c5521fab1a1d444da96768a6e
kotinash <92477814+kotinash@users.noreply.github.com>
2024-02-11 13:49:29 +0000

    DisconnectionMessage

---
+++ src/player/DisconnectionMessage.ts
@@ -0,0 +1,25 @@
+enum Message {
+    ServerFull = "disconnectionScreen.serverFull",
+    NoReason = "disconnectionScreen.noReason"
+}
+
+class DisconnectionMessage {
+    public message: string
+
+    constructor(_message: string) {
+        this.message = _message
+    }
+
+    public toString() {
+        switch (this.message) {
+            case Message.ServerFull:
+                return "Wow this server is popular! Check back later to see if space opens up."
+            case Message.NoReason:
+                return "You were disconnected"
+            default:
+                throw new Error("Unsupported message: " + this.message)
+        }
+    }
+}
+
+export { DisconnectionMessage }
