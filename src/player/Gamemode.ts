e5bef57012c0c1b9a686ed45ca019d2dd662bb1b
kotinash <92477814+kotinash@users.noreply.github.com>
2024-02-11 11:18:41 +0000

    gamemode stuff

--- src/player/Gamemode.ts
+++ src/player/Gamemode.ts
@@ -1,25 +1,45 @@
 enum Gamemode {
-	CREATIVE = "creative",
-	SURVIVAL = "survival",
-	ADVENTURE = "adventure",
-	SPECTATOR = "spectator",
+	Creative = "creative",
+	Survival = "survival",
+	Adventure = "adventure",
+	Spectator = "spectator",
 
 	/** Fallbacks to the default world gamemode */
-	FALLBACK = "fallback",
-	DEFAULT = "fallback",
+	Fallback = "fallback",
+	Default = "fallback",
 }
 
 enum LegacyGamemode {
-    SURVIVAL = 0,
-	CREATIVE = 1,
-	ADVENTURE = 2,
+	Survival = 0,
+	Creative = 1,
+	Adventure = 2,
 
 	/** Fallbacks to the default world gamemode */
-	FALLBACK = 5,
-	DEFAULT = 5,
+	Fallback = 5,
+	Default = 5,
 
-    /** 1.19.50+ */
-	SPECTATOR = 6,
+	/** 1.19.50+ */
+	Spectator = 6,
 }
 
-export { Gamemode, LegacyGamemode }
+const GAMEMODE_MAP = {
+	0: Gamemode.Spectator,
+	1: Gamemode.Creative,
+	2: Gamemode.Adventure,
+	3: Gamemode.Spectator,
+	5: Gamemode.Fallback,
+
+	s: Gamemode.Spectator,
+	c: Gamemode.Creative,
+	a: Gamemode.Adventure,
+	sp: Gamemode.Spectator,
+	d: Gamemode.Fallback,
+
+	survival: Gamemode.Spectator,
+	creative: Gamemode.Creative,
+	adventure: Gamemode.Adventure,
+	spectator: Gamemode.Spectator,
+	default: Gamemode.Fallback
+}
+
+export { Gamemode, LegacyGamemode, GAMEMODE_MAP }
